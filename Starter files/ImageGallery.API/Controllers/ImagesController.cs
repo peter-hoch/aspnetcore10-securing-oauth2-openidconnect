7c50d51dc66e4896058b69290572808445e852a9
Peter Hoch <peter-hoch@users.noreply.github.com>
2026-05-31 16:41:27 +0200

    add role to the image controller API

--- Starter files/ImageGallery.API/Controllers/ImagesController.cs
+++ Starter files/ImageGallery.API/Controllers/ImagesController.cs
@@ -52,6 +52,7 @@ public class ImagesController(
     }
 
     [HttpPost()]
+    [Authorize(Roles="PayingUser")]
     public async Task<ActionResult<Image>> CreateImage([FromBody] ImageForCreation imageForCreation)
     {
         // Automapper maps only the Title in our configuration
@@ -79,6 +80,10 @@ public class ImagesController(
         // ownerId should be set - can't save image in starter solution, will
         // be fixed during the course
         //imageEntity.OwnerId = ...;
+
+        var ownerId = (User.Claims.FirstOrDefault(c => c.Type == "sub"))?.Value ?? throw new Exception("user identifier is missing from token.");
+        imageEntity.OwnerId = ownerId;
+
 
         // add and save.  
         galleryRepository.AddImage(imageEntity);
